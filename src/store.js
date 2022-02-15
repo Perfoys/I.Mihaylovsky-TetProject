71dd69b202d566ddf5a9d506589c277adc189302
Perfoys <prorok43battle@gmail.com>
2022-02-15 12:02:46 +0100

    Created store for redux with initial state

---
+++ src/store.js
@@ -0,0 +1,15 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import combineReducers from './reducer';
+
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const composedEnhancer = composeWithDevTools(applyMiddleware(...middleware));
+
+const store = createStore(combineReducers, initialState, composedEnhancer);
+
+export default store;
