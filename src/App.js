bab99da5a867549b3a9e127110f8ec9ba5d559f7
Perfoys <prorok43battle@gmail.com>
2022-02-15 12:04:05 +0100

    Connected redux store to App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 
+import { Provider } from 'react-redux';
+import store from './store';
+
 function App () {
   return (
-    <div className="App">
-      Test App
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        Test App
+      </div>
+    </Provider>
   );
 }
 
