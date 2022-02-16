4d87d2c39210ce73ec54f19a5fa7b15d5af82933
Perfoys <prorok43battle@gmail.com>
2022-02-16 13:50:44 +0100

    Added data for dynamic rendering into HomePage component

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,10 +2,25 @@ import Header from '../components/header/Header';
 import MembersTable from '../components/table/MembersTable';
 
 const HomePage = () => {
+  const defaultSprint = {
+    title: 'Sprint title',
+    startDate: new Date('2022-02-02'),
+    endDate: new Date('2022-02-25'),
+    toDoTasks: [
+      {
+        id: 1,
+        title: 'TASK',
+        priority: 'High',
+        description: 'Apriel inventory of supplies',
+        img: 'https://www.kindpng.com/picc/m/130-1300217_user-icon-member-icon-png-transparent-png.png'
+      }
+    ]
+  };
+
   return (
         <div>
-            <Header></Header>
-            <MembersTable></MembersTable>
+            <Header sprint={defaultSprint}></Header>
+            <MembersTable toDoTasks={defaultSprint.toDoTasks}></MembersTable>
         </div>
   );
 };
