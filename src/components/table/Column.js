fcaa15e7981794a5140a7d40d505a55d1d7d5006
Perfoys <prorok43battle@gmail.com>
2022-02-22 13:12:41 +0100

    Modified Column component, Created state, Created onDrop and moveCard functions

--- src/components/table/Column.js
+++ src/components/table/Column.js
@@ -1,14 +1,44 @@
 import PropTypes from 'prop-types';
+import update from 'immutability-helper';
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { changeTaskStatus } from '../../reducers/sprintReducer';
 import TaskCard from './TaskCard';
 import style from './column.module.scss';
+import DropWrapper from './DropWrapper';
 
 const Column = ({ title, tasks }) => {
+  const [cards, setCards] = useState(tasks);
+  const dispatch = useDispatch();
+  const moveCard = (dragIndex, hoverIndex) => {
+    setCards(prevCards => update(prevCards, {
+      $splice: [
+        [dragIndex, 1],
+        [hoverIndex, 0, prevCards[dragIndex]]
+      ]
+    }));
+  };
+
+  const onDrop = (item, monitor, title) => {
+    if (item.status === title) return;
+    dispatch(changeTaskStatus({ item: item, title: title }));
+    setCards(prevCards => update(prevCards, {
+      $push: [item]
+    }));
+  };
+
+  useEffect(() => {
+    setCards(tasks);
+  }, [tasks]);
+
   return (
     <div className={style.column}>
       <h1 className={style.title}>{title}</h1>
-      {tasks.map(item => (
-        <TaskCard key={item.id} task={item} />
-      ))}
+      <DropWrapper onDrop={onDrop} title={title}>
+        {cards.map((item, index) => (
+          item && <TaskCard key={item.id} task={item} index={index} moveCard={moveCard} />
+        ))}
+      </DropWrapper>
     </div>
   );
 };
