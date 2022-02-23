9a4382cf260111de48441f9d285f46af1cd8d7f5
Perfoys <prorok43battle@gmail.com>
2022-02-23 13:23:10 +0100

    Used useCallback to wrap onDrop and moveCard functions, deleted component state

--- src/components/table/Column.js
+++ src/components/table/Column.js
@@ -1,44 +1,33 @@
 import PropTypes from 'prop-types';
-import update from 'immutability-helper';
 import classNames from 'classnames';
-import { useEffect, useState } from 'react';
+import { useCallback } from 'react';
 import { useDispatch } from 'react-redux';
-import { changeTaskStatus } from '../../reducers/sprintReducer';
+import { changeTaskStatus, changeTaskOrder } from '../../reducers/sprintReducer';
 import TaskCard from './TaskCard';
 import style from './column.module.scss';
 import DropWrapper from './DropWrapper';
 
 const Column = ({ title, titleStyle, tasks }) => {
-  const [cards, setCards] = useState(tasks);
   const dispatch = useDispatch();
 
-  const moveCard = (dragIndex, hoverIndex) => {
-    setCards(prevCards => update(prevCards, {
-      $splice: [
-        [dragIndex, 1],
-        [hoverIndex, 0, prevCards[dragIndex]]
-      ]
-    }));
-  };
+  const moveCard = useCallback((dragIndex, hoverIndex, status) => {
+    if (status === title) {
+      dispatch(changeTaskOrder({ dragIndex: dragIndex, hoverIndex: hoverIndex }));
+    }
+  }, [tasks]);
 
-  const onDrop = (item, monitor, title) => {
-    if (item.status === title) return;
-    dispatch(changeTaskStatus({ item: item, title: title }));
-    setCards(prevCards => update(prevCards, {
-      $push: [item]
-    }));
-  };
-
-  useEffect(() => {
-    setCards(tasks);
+  const onDrop = useCallback((item, monitor, title) => {
+    if (item.status !== title) {
+      dispatch(changeTaskStatus({ item: item, title: title }));
+    }
   }, [tasks]);
 
   return (
     <div className={style.column}>
       <h1 className={classNames(style.title, titleStyle)}>{title}</h1>
       <DropWrapper onDrop={onDrop} title={title}>
-        {cards.map((item, index) => (
-          item && <TaskCard key={item.id} task={item} index={index} moveCard={moveCard} />
+        {tasks.map(task => (
+          task && <TaskCard key={task.id} task={task} moveCard={moveCard} />
         ))}
       </DropWrapper>
     </div>
