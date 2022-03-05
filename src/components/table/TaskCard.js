5d810504d02f02cc1ec982f5ef1f151e555527ed
Perfoys <prorok43battle@gmail.com>
2022-03-05 19:40:10 +0100

    Fixed problems into TaskCard component

--- src/components/table/TaskCard.js
+++ src/components/table/TaskCard.js
@@ -7,10 +7,7 @@ import Timer from '../common/Timer';
 import style from './taskCard.module.scss';
 import TaskDetail from '../modal/TaskDetail';
 
-const TaskCard = ({
-  task: { id, status, title, priority, description, img, deadline, member, comments },
-  moveCard
-}) => {
+const TaskCard = ({ task, moveCard }) => {
   const ref = useRef(null);
   const [styleCard, setStyleCard] = useState(style.card);
   const [showModal, setShowModal] = useState(false);
@@ -18,22 +15,21 @@ const TaskCard = ({
   const [, drop] = useDrop(() => ({
     accept: ItemTypes.CARD,
     hover: (item, monitor) => {
-      if (item.id !== id) {
-        moveCard(item.id, id, item.status);
+      if (item.id !== task.id) {
+        moveCard(item.id, task.id, item.status);
       }
     }
   }));
 
   const [{ isDragging }, drag] = useDrag(() => ({
     type: ItemTypes.CARD,
-    item: { id, status },
+    item: { id: task.id, status: task.status },
     collect: monitor => ({
       isDragging: monitor.isDragging()
     })
   }));
 
   const openModal = () => setShowModal(true);
-
   const closeModal = () => setShowModal(false);
 
   useEffect(() => {
@@ -48,20 +44,16 @@ const TaskCard = ({
     <>
       <div className={styleCard} ref={ref} onClick={openModal}>
         <div className={style.info}>
-          <h2 className={style.title}>{id}-{title}</h2>
+          <h2 className={style.title}>{task.id}-{task.title}</h2>
           <div className={style.description}>
-            <div className={classNames(style.block, style.priority)}>{priority}</div>
-            <div className={style.block}>{description}</div>
+            <div className={classNames(style.block, style.priority)}>{task.priority}</div>
+            <div className={style.block}>{task.description}</div>
           </div>
-          <Timer text={'Time left (days)'} endDate={deadline} />
+          <Timer text={'Time left (days)'} endDate={task.deadline} />
         </div>
-        <div className={style.img}><img src={img} alt='avatar'></img></div>
+        <div className={style.img}><img src={task.image} alt='avatar'></img></div>
       </div>
-      <TaskDetail
-        task={{ id: id, title: title, status: status, description: description, member: member, comments: comments }}
-        isModalOpen={showModal}
-        closeModal={closeModal}
-      />
+      <TaskDetail task={task} isModalOpen={showModal} closeModal={closeModal} />
     </>
   );
 };
